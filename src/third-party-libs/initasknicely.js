691c0a211055e5ef4e9d039e9aa77c208b1f61fc
Mehul Shah <mehul.shah@brevitaz.com>
2018-11-09 19:51:37 +0530

    initial commit

---
+++ src/third-party-libs/initasknicely.js
@@ -0,0 +1,7 @@
+export const initAsknicely = () => {
+	window.asknicelySettings = {
+    	autoload: false,
+    	domain_key: "nassauvisiongroup",  // Domain Key - required
+    	force: 1  // Only set this true for testing to the popup so it will ALWAYS appear
+    };
+}
